c7a2caed498dad79c73061140fa11877e13a1476
Liliana Tchernev <liliana.tchernev@merton.ox.ac.uk>
2023-11-07 15:00:43 +0000

    First Commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("/cloud/project/experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,18 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 7837379.9223475 #
   
-r <- ??? #
+r <- 0.0100086 #
   
-K <- ??? #
+K <- 60000000000 #
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point()+
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
 
 
