51b9f655d7f58792fc4f2efb69ad100eac9a319f
tgbhu <150151014+tgbhujm@users.noreply.github.com>
2023-12-07 20:16:43 +0000

    Update plot_data_and_model.R

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -2,6 +2,7 @@
 
 growth_data <- read.csv("/cloud/project/experiment1.csv")
 
+# Creating a function to calculate N assuming logistic growth:
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
@@ -10,12 +11,13 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- 7837379.9223475 #
+N0 <- 986.5074723094 #represented by the power of the log that equals the intercept value in the output of model1. 
   
-r <- 0.0100086 #
+r <- 0.0100086 # represented by the slope value in the output of model1.
   
-K <- 60000000000 #
+K <- 60000000000 # represented by the intercept value in the output of model2.
 
+# Plotting the logistic growth of E.coli population size:
 ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
