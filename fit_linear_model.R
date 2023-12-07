b969e9884bef177f1d90f4745351e804773af3be
tgbhu <150151014+tgbhujm@users.noreply.github.com>
2023-12-07 20:10:21 +0000

    Update fit_linear_model.R

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -11,7 +11,9 @@ data_subset1 <- growth_data %>% filter(t<1500) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
-#significant => this model fits this part of the data well?
+# p<<<0.05 => significant.
+# R^2=1 => 100% of the variability in population size is explained by the linear model above.
+# The natural log of the initial population size (ln(N0)) is the intercept, and the growth rate (r) is the slope in this output.
 
 #Case 2. N(t) = K
 
@@ -19,4 +21,4 @@ data_subset2 <- growth_data %>% filter(t>2500)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
-#significant => this other model fits this other part of the data well?
+# The carrying capacity (K) estimate is the intercept in this output.
